6a0b3a55419e462a584b8da9f34dc78fffe1967c
Akong Rodney <rodneyakong@gmail.com>
2025-05-22 23:07:56 +0100

    created a program to calculate the area of a triangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+'''
+Program to find the area of a rectangle with the formular;
+area = length * Width
+Author: Akong Rodney
+Date : 22/05/2025
+'''
+length = 10 
+width = 5
+area = length * width
+print("The area of the rectangle is:", area)
