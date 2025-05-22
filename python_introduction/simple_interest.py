429a006c9a60b60bef834137c6b641faa307012e
Akong Rodney <rodneyakong@gmail.com>
2025-05-22 22:52:54 +0100

    Created a simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+'''
+The programe calculates simple interest where;
+I = interest
+p = principal
+t= time
+r = rate.
+Author: Akong Rodney
+Date: 22/05/2025
+'''
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+print("The simple interest is:", interest)
