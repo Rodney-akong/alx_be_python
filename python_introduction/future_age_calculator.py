7c28f612d1786766215335db8ea4bf01a167846a
Akong Rodney <rodneyakong@gmail.com>
2025-05-23 03:32:43 +0100

    made a program to predict the future age of a use

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+'''
+Program to calcukate the future age of a user
+Author: Akong Rodney
+DAte: 23/05/2025
+'''
+
+age = int(input("Please input your current age: "))
+new_age = age + 27
+print("In 2050, you will be", new_age, "years old")
