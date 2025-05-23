56745fe042e4ecf115acd504cde635c52ffd3be9
Akong Rodney <rodneyakong@gmail.com>
2025-05-23 03:04:27 +0100

    create a time convertion from hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+''''
+Program to convert a certain number of hours to seconds
+Author: Akong Rodney 
+Date:23/05/2025
+'''
+hours = 2
+convertion = 60 * 60
+seconds = hours * convertion
+
+print(hours, "hour(s)", "is", seconds,"seconds.")
