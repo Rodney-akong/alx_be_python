ed22028a1c16b7a508ffb9827a69bbc012331644
Akong Rodney <rodneyakong@gmail.com>
2025-06-01 09:59:56 +0100

    modified the multiplication table using for loop, there wasain issue with the print statement

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -5,11 +5,11 @@ then use a for loop to print the multiplication table for that number from 1 to 
 '''
 # Take input from the user.
 
-number = int(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table: "))
 #Use a for loop to loop through the range of 10 integers
 
 for i in range(1, 11):
 
     y = number * i
-    
-    print(number, "*", i , "=", y)
+
+    print(number, "*", i, "=", y)
