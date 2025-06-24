5bcf325169dd6d350f39171ecc33a1652860fcca
Akong Rodney <rodneyakong@gmail.com>
2025-06-24 18:24:29 +0100

    magic method functionality

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,24 +8,24 @@ Author: Akong Rodney
 Date: 24/06/2025
 '''
 class Book:
-    def __init__(self,title,author,year):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
 
+    def __del__(self):
+        print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f'{self.title} by {self.author}, published in {self.year}'
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    def __del__(self):
-        return f"Deleting {self.title}"
 
 if __name__ == "__main__":
-    book = Book("1984", "George Orwell", 1949)
-    print(str(book))
-    print(repr(book))
-    del book
+    book1 = Book("1984", "George Orwell", 1949)
+    print(str(book1))
+    print(repr(book1))
+    del book1
     
