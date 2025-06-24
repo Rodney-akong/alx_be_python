0a2227e05b472092e8958edf70b5429cd4640db4
Akong Rodney <rodneyakong@gmail.com>
2025-06-24 17:28:39 +0100

    magic method functionality

---
+++ oop/book_class.py
@@ -0,0 +1,30 @@
+'''
+Task Description:
+
+Create a Python script named book_class.py. 
+In this script, define a Book class that uses specific magic methods to enhance its functionality. #
+This class will model a book with attributes for the title, author, and publication year.
+Author: Akong Rodney
+Date: 24/06/2025
+'''
+class Book:
+    def __init__(self,title,author,year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def __str__(self):
+        return f'{self.title} by {self.author}, published in {self.year}'
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})."
+
+    def __del__(self):
+        return f"deleeting {self.title}"
+
+
+book = Book()
+print(book.__str__())
+print(book.__repr__())
+print(book.__del__())
